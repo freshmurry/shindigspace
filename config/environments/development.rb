47cbdcb2146d1c6f80f66970cd4be2663ab37682
Ubuntu <ubuntu@ip-172-31-27-189.ec2.internal>
2020-07-29 02:40:10 +0000

    Added mailer host

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -67,7 +67,7 @@ Rails.application.configure do
   # config.web_console.whitelisted_ips << "10.240.0.0/16"
   
   #Required for Heroku
-  config.action_mailer.default_url_options = { host: 'https://37e5371448fa4b70b49ca2697e0a1b75.vfs.cloud9.us-east-1.amazonaws.com' }
+  config.action_mailer.default_url_options = { host: 'https://poolcrasher.herokuapp.com' }
   
   #Mailgun Integration
   
