18e8c2b96aa0351d00a718601b5bc2866e607c32
Lawrence <lawrencemurry@yahoo.com>
2020-08-03 18:51:43 +0000

    Fixed assets file extentions

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -49,9 +49,8 @@ Rails.application.configure do
   # config.action_cable.mount_path = nil
   # config.action_cable.url = 'wss://example.com/cable'
   # config.action_cable.allowed_request_origins = [ 'http://example.com', /http:\/\/example.*/ ]
-  # config.web_socket_server_url = "wss://action-cable-poolcrasher.herokuapp.com/cable"
-  # config.action_cable.allowed_request_origins = ['https://action-cable-poolcrasher.herokuapp.com/cable', 'https://action-cable-poolcrasher.herokuapp.com/cable' ]
-
+  config.web_socket_server_url = "wss://action-cable-poolcrasher.herokuapp.com/cable"
+  config.action_cable.allowed_request_origins = ['https://action-cable-poolcrasher.herokuapp.com/cable', 'https://action-cable-poolcrasher.herokuapp.com/cable' ]
 
   # Force all access to the app over SSL, use Strict-Transport-Security, and use secure cookies.
   # config.force_ssl = true
