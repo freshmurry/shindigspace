264792e1ba4cbec04755de900b2d449efc36c0b9
Lawrence <lawrencemurry@yahoo.com>
2020-08-03 04:18:55 +0000

    Whitelisted FB Credentials

--- config/initializers/devise.rb
+++ config/initializers/devise.rb
@@ -275,8 +275,9 @@ Devise.setup do |config|
   # so you need to do it manually. For the users scope, it would be:
   # config.omniauth_path_prefix = '/my_engine/users/auth'
 
-  config.omniauth :facebook, '401108474197132', '40a8684b78f2efba69d991a1affb7da8', scope: 'email', info_fields: 'email, name'
-  # config.omniauth :facebook, ENV['FB_APP_ID'], ENV['FB_APP_SECRET'], callback_url: 'https://shindigspace.com/auth/facebook'
+  # config.omniauth :facebook, '', '', scope: 'email', info_fields: 'email, name'
+  config.omniauth :facebook, ENV['FB_APP_ID'], ENV['FB_APP_SECRET'], scope: 'email', info_fields: 'email, name'
+  # config.omniauth :facebook, ENV['FB_APP_ID'], ENV['FB_APP_SECRET'], callback_url: 'https://poolcrasher.heroku.com/auth/facebook'
                 # client_options: {
                 #     site: 'https://graph.facebook.com/v2.11',
                 #     authorize_url: "https://www.facebook.com/v2.11/dialog/oauth"
