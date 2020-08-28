d692615019efbccbf64319e98595990f9e8e8d65
Lawrence <lawrencemurry@yahoo.com>
2020-08-28 07:42:41 +0000

    Deleted code

--- app/models/message.rb
+++ app/models/message.rb
@@ -1,8 +1,6 @@
 class Message < ApplicationRecord
   belongs_to :user
   belongs_to :conversation
-  has_many :conversations, dependent:  :destroy
-
   
   validates_presence_of :context, :conversation_id, :user_id
   after_create_commit :create_notification
--- app/models/notification.rb
+++ app/models/notification.rb
@@ -1,5 +1,4 @@
 class Notification < ApplicationRecord
   after_create_commit { NotificationJob.perform_later self }
-  has_many :notifications, dependent: :destroy
   belongs_to :user
 end
