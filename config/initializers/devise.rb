95ad63600f073ee2e1809fdc89d0ba67ba6f9cac
Lawrence <lawrencemurry@yahoo.com>
2020-09-11 19:17:18 +0000

    Updated FB auth

--- config/initializers/devise.rb
+++ config/initializers/devise.rb
@@ -277,7 +277,7 @@ Devise.setup do |config|
 
   # config.omniauth :facebook, '', '', scope: 'email', info_fields: 'email, name'
   # config.omniauth :facebook, ENV['FB_APP_ID'], ENV['FB_APP_SECRET'], scope: 'email', info_fields: 'email, name'
-  config.omniauth :facebook, ENV['FB_APP_ID'], ENV['FB_APP_SECRET'], callback_url: 'www.shindigspace.com/auth/facebook/callback'
+  config.omniauth :facebook, ENV['FB_APP_ID'], ENV['FB_APP_SECRET'], callback_url: 'https://shindigspace.herokuapp.com/auth/facebook/callback'
                 # client_options: {
                 #     site: 'https://graph.facebook.com/v2.11',
                 #     authorize_url: "https://www.facebook.com/v2.11/dialog/oauth"
