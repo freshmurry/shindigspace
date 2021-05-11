5bce62981821b76245f5b52ee1035c6a6b3312d1
Lawrence <lawrencemurry@yahoo.com>
2021-05-11 17:05:05 +0000

    Configured for SSL

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -53,7 +53,7 @@ Rails.application.configure do
   config.action_cable.allowed_request_origins = ['https://action-cable-shindigspace.herokuapp.com/cable', 'https://action-cable-shindigspace.herokuapp.com/cable' ]
 
   # Force all access to the app over SSL, use Strict-Transport-Security, and use secure cookies.
-  # config.force_ssl = true
+  config.force_ssl = true
 
   # Use the lowest log level to ensure availability of diagnostic information
   # when problems arise.
@@ -93,7 +93,7 @@ Rails.application.configure do
   config.action_mailer.raise_delivery_errors = true
   
     config.action_mailer.delivery_method = :smtp
-    host = 'shindigspace.herokuapp.com'
+    host = 'www.shindigspace.com'
     config.action_mailer.default_url_options = { host: host }
     ActionMailer::Base.smtp_settings = {
       :address        => 'smtp.sendgrid.net',
